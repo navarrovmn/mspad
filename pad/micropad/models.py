8c99435d46cd38633e31049716cf6ee11ed237b1
Victor Navarro <victor.matias.navarro@gmail.com>
2018-06-09 13:45:19 -0300

    Adding monaco, endpoints and creations of objects

--- pad/micropad/models.py
+++ pad/micropad/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from boogie.rest import rest_api
 
+@rest_api()
+class Folder(models.Model):
+    """
+    Represents a folder.
+    """
+
+    name = models.CharField(max_length=50)
+    relates_to = models.ForeignKey('self', on_delete=models.CASCADE)
 
 @rest_api()
 class File(models.Model):
     """
     Represents a file.
     """
+    folder = models.ForeignKey(Folder, related_name="files", on_delete=models.CASCADE)
     text = models.TextField()
     name = models.CharField(max_length=50)
     url = models.CharField(max_length=140)
--- pad/micropad/views.py
+++ pad/micropad/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
-from . import models
+from .models import *
 
 
 def editor(request, path, name, ext):
+    last_father = ""
+
+    if path is not None:
+        folders = path.split('/')
+        print(folders)
+
+        for idx, folder in enumerate(folders):
+            try:
+                get_folder = Folder.objects.get(name=folder)
+            except Folder.DoesNotExist:
+                if idx==0:
+                    get_folder = Folder.objects.create(name=folder)
+                else:
+                    get_folder = Folder.objects.create(name=folder, folder=Folder.objects.get(name=last_father))
+
+            last_father = folder
+
+    try:
+        archive = File.objects.get(name=name)
+    except File.DoesNotExist:
+        archive = File.objects.create(text="", name=name, url=path+name+ext, folder=Folder.objects.get(name=last_father))
+
     ctx = {
-        'path': path,
-        'name': name,
-        'extension': ext,
+        'archive': archive
     }
-    print("EDITOR")
+
     return render(request, 'micropad/editor.html', ctx)
+
 
 def folder_list(request, path):
     ctx = {
